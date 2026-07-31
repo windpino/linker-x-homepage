5ffe7b3c766e0bf4037695439b3bf4bf61b4e4e0
windpino <windpino@gmail.com>
2026-08-01 02:03:01 +0900

    feat: Register sw.js service worker to trigger active PWA install prompt

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass-through to network
+  event.respondWith(fetch(event.request));
+});
