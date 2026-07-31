735330ffb76508e25cf36ebf201c4adf4abe6951
windpino <windpino@gmail.com>
2026-08-01 01:13:06 +0900

    fix: Unconditionally set base path to absolute root for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env variables from system process & env files
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  // Cross check if Vercel build environment is active
-  const isVercel = 
-    env.VERCEL === '1' || 
-    env.VERCEL === 'true' || 
-    process.env.VERCEL === '1' || 
-    process.env.VERCEL === 'true' ||
-    process.env.VERCEL_ENV !== undefined;
-
-  return {
-    plugins: [react()],
-    base: isVercel ? '/' : './',
-    server: {
-      port: 5173,
-      strictPort: true,
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
 })
